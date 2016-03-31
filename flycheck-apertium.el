7a17ffc6bfc5b0b4a279fe4690015d13586dfd69
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-03-31 11:33:30 +0200

    nicer usage doc

--- flycheck-apertium.el
+++ flycheck-apertium.el
@@ -17,10 +17,15 @@
 ;; (apertium-all-dev) from the nightly repos:
 ;; http://wiki.apertium.org/wiki/Installation
 
-;; To use it, add to your init.el:
+;; To use it, add this to your init.el:
 
-;; (require 'flycheck-apertium)
-;; (add-hook 'nxml-mode-hook 'flycheck-mode)
+;; (when (locate-library "flycheck-apertium")
+;;   (require 'flycheck-apertium)
+;;   (add-hook 'nxml-mode-hook 'flycheck-mode))
+
+;; If not installing through ELPA, you'll also have to do
+
+;; (add-to-list 'load-path "/path/to/flycheck-apertium-directory/")
 
 ;;; License:
 
